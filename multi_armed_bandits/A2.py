8d8c8a79fe52f5d320147079c1ce33e39d5861dc
kutayeken55 <99044189+kutayeken55@users.noreply.github.com>
2025-01-31 12:37:45 -0700

    added plots and updated code

--- multi_armed_bandits/A2.py
+++ multi_armed_bandits/A2.py
@@ -5,12 +5,6 @@ import matplotlib.pyplot as plt
 # Author: Kutay Eken
 # Date: Jan 29, 2025
 
-# PT1:
-# - only plot the average rewards, not the optimal action
-# - plot lines for epsilon 0, 0.01, and 0.1
-# - use numpy.random.normal for pt1 (reward sampling)
-# - number of bandit tasks should be at least 100
-# - discuss assumptions and results
 class Bandit():
     # intializes the multi-arm bandit
     def __init__(self, num_actions):
@@ -40,8 +34,67 @@ class Bandit():
 
         return new_reward
 
+class Bernoulli_Bandit():
+    # intializes the multi-arm bandit
+    def __init__(self, num_actions, greedy):
+        self.action_ids = list(range(num_actions))
+        self.action_to_probs = {key: np.random.uniform(0, 1) for key in self.action_ids}
+        self.action_to_rewards = {key: [] for key in self.action_ids}
+        self.action_to_counts = {key: 0 for key in self.action_ids}
+        self.Q_t = {key: 0 for key in self.action_ids}
+        
+        if greedy == False:
+            # pull each arm once
+            for action_id in self.action_ids:
+                init_reward = np.random.binomial(1, self.action_to_probs[action_id])
+                self.action_to_rewards[action_id].append(init_reward)
+                self.Q_t[action_id] = init_reward
+                self.action_to_counts[action_id] += 1
+
+    def calculate_c(self, action_id, t):
+        return np.sqrt((np.log(t + 1)) / (self.action_to_counts[action_id] + 1e-5))
+
+    def take_action_bern(self, time_step):
+        ucb_values = np.array([self.Q_t[a] + self.calculate_c(a, time_step) for a in self.action_ids])
+        action_taken = np.argmax(ucb_values)
+
+        new_reward = np.random.binomial(1, self.action_to_probs[action_taken])
+
+        self.action_to_counts[action_taken] += 1
+
+        self.action_to_rewards[action_taken].append(new_reward)
+
+        new_estimate_reward = sum(self.action_to_rewards[action_taken]) / self.action_to_counts[action_taken]
+
+        self.Q_t[action_taken] = new_estimate_reward
+
+        return new_reward
+
+    def take_action_greedy(self, epsilon, time_step):
+        x = np.random.uniform(0, 1)
+
+        if x <= epsilon:
+            # take random action
+            action = random.choice(self.action_ids)
+        else:
+            # greedy action
+            action = max(self.Q_t, key=self.Q_t.get)
+        
+        new_reward = np.random.binomial(1, self.action_to_probs[action])
+
+        self.action_to_rewards[action].append(new_reward)
+
+        self.action_to_counts[action] += 1
+
+        new_estimate_reward = sum(self.action_to_rewards[action]) / self.action_to_counts[action]
+
+        self.Q_t[action] = new_estimate_reward
+
+        return new_reward
+
 # creates 2000 10-armed bandits. Each agent plays 1000 times. Computes average reward for each time_step t. Plots the results to compare different epsilon values.
 def run_experiment(epsilon_vals=[0, 0.01, 0.1], plays=1000):
+    print("Started Epsilon Greedy Experiment")
     results = []
     for eps in epsilon_vals:
         eps_t_reward_avg = {key: 0 for key in range(plays)}
@@ -54,8 +107,45 @@ def run_experiment(epsilon_vals=[0, 0.01, 0.1], plays=1000):
         eps_t_reward_avg = {key: value / 2000 for key, value in eps_t_reward_avg.items()}
         results.append(eps_t_reward_avg)
 
-    print("Experiment is finished. Now Plotting...")
+    print("Finished Epsilon Greedy Experiment")
     
+    return results
+
+# Compares Bernoulli Bandit Performance Between UBC1 and Epsilon Greedy (0, 0.01, 0.1)
+def run_experiment_bernoulli(epsilon_vals=[0, 0.01, 0.1], plays=1000):
+    print("Started Bernoulli Bandit Experiment...")
+    results = []
+
+    # eps Greedy w/ Bernouilli Bandit
+    for eps in epsilon_vals:
+        eps_t_reward_avg = {key: 0 for key in range(plays)}
+        for i in range(2000):
+            bandit = Bernoulli_Bandit(10, True)
+            for t in range(plays):
+                reward = bandit.take_action_greedy(eps, t)
+                eps_t_reward_avg[t] += reward
+
+        eps_t_reward_avg = {key: value / 2000 for key, value in eps_t_reward_avg.items()}
+        results.append(eps_t_reward_avg)
+
+
+    # UBC1
+    time_to_avg_rew = {key: 0 for key in range(plays)}
+    for i in range(2000):
+        bern_bandit = Bernoulli_Bandit(10, False)
+        for t in range(plays):
+            reward = bern_bandit.take_action_bern(t)
+            time_to_avg_rew[t] += reward
+    
+    time_to_avg_rew = {key: value / 2000 for key, value in time_to_avg_rew.items()}
+    print("Finished Bernoulli Bandit Experiment...")
+
+    return results, time_to_avg_rew
+
+# Plots Greedy Epsilon Comparison
+def plot_eps(results, epsilon_vals=[0, 0.01, 0.1]):
+    print("Plotting the Greedy Epsilon...")
+
     plt.figure(figsize=(8,5))
 
     for i, data in enumerate(results):
@@ -70,16 +160,32 @@ def run_experiment(epsilon_vals=[0, 0.01, 0.1], plays=1000):
     plt.legend()
     plt.show()
 
+# Plots Greedy Epsilon vs UBC1
+def plot_all(results, ubc_results, epsilon_vals=[0, 0.01, 0.1]):
+    print("Plotting Greedy Epsilon & UBC1 Comparison")
 
-run_experiment()
+    plt.figure(figsize=(8,5))
 
+    for i, data in enumerate(results):
+        epsil = epsilon_vals[i]
+        x = list(data.keys())
+        y = list(data.values())
+        plt.plot(x, y, marker='.', label=f'ε = {epsil}')
+    
+    ubc_x = list(ubc_results.keys())
+    ubc_y = list(ubc_results.values())
+    plt.plot(ubc_x, ubc_y, marker='.', label="UBC1")
 
-# PT2:
-# - have 10 arms
-# - Bernoulli bandit (+1 with prob. p, 0 with prob. 1-p)
-# - use epsilon = 0, 0.01, 0.1 with UCB1??
-# - average your results over 100 random bandit problems
-# - To create each problem, sample a probability p for each arm uniformly in the range [0,1]
-# - use numpy.random.binomial for pt2
-# - Discuss the results
+    plt.xlabel("Plays")
+    plt.ylabel("Avg. Reward")
+    plt.title("Multi-Armed Bandit Comparison of ε-Greedy and UBC1 Algorithms")
+    plt.legend()
+    plt.show()
 
+# PART 1
+results = run_experiment()
+plot_eps(results)
+
+# PART 2
+results, ubc_results = run_experiment_bernoulli()
+plot_all(results, ubc_results)
